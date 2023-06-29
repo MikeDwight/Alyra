0dfd469ffe4f2dca3507df26a790638535a685dd
Mike <mickael.ha@gmai.com>
2023-06-29 16:32:13 +0200

    Va t-on reussir à afficher ce data ?

--- DAPP/TP03/frontend/components/Voting/Voting.js
+++ DAPP/TP03/frontend/components/Voting/Voting.js
@@ -7,7 +7,7 @@ import { Flex, Text, Heading, Input, Button, useToast } from '@chakra-ui/react'
 
 // Wagmi
 import { useAccount } from 'wagmi'
-import { prepareWriteContract, writeContract } from '@wagmi/core'
+import { prepareWriteContract, writeContract, readContract } from '@wagmi/core'
 
 // Contract
 import Contract from '../../../backend/artifacts/contracts/Voting.sol/Voting.json'
@@ -26,15 +26,18 @@ const Voting = () => {
 
     // STATES
     const [addVoter, setAddVoter] = useState(null)
+    const [getVoter, setGetVoter] = useState(null)
+    const [data, setData] = useState(null)
 
     // CONTRACT ADDRESS
     const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS
 
 
     // COMPORTEMENT
+
+    // Fonction pour ajouter un voter
     const addOneVoter = async() => {
         try {
-            console.log(addVoter);  
             const { request } = await prepareWriteContract({
                 address: contractAddress,
                 abi: Contract.abi,
@@ -63,10 +66,28 @@ const Voting = () => {
         }
     }
 
+    const getInfoVoter = async() => {
+        try {
+            const data = await readContract({
+                address: contractAddress,
+                abi: Contract.abi,
+                functionName: "getVoter",
+                args: [getVoter],
+                account: getVoter,           
+            })
+            console.log(data);
+            return data
+            setData(data)
+        } catch (err) {
+            console.log(err.message)
+        }
+    }
+
+
 
     // AFFICHAGE
   return (
-    <Flex w={'60%'} bg={'#6B4E71'} m={'auto'} p={'50px'}  justifyContent={'center'} alignItems={'center'}>
+    <Flex w={'60%'} bg={'#6B4E71'} color={'#F5DDDD'} m={'auto'} p={'50px'}  justifyContent={'center'} alignItems={'center'}>
         {isConnected ? (
             <Flex direction={'column'} width={'100%'}>
                 <Heading as={'h1'} size={'xl'}>
@@ -75,6 +96,14 @@ const Voting = () => {
                 <Flex m={'15px'}>
                     <Input placeholder='Entrez une adresse' onChange={e => setAddVoter(e.target.value)}></Input>
                     <Button onClick={() => addOneVoter()}>Ajouter</Button>
+                </Flex>
+                <Heading as={'h1'} size={'xl'}>
+                    Obtenir les informations d'un voter
+                </Heading>
+                <Flex m={'15px'}>
+                    <Input placeholder='Entrez une adresse' onChange={e => setGetVoter(e.target.value)}></Input>
+                    <Button onClick={() => getInfoVoter()}>Information</Button>
+                    <Text>{data}</Text>
                 </Flex>
             </Flex>
         ) : (
