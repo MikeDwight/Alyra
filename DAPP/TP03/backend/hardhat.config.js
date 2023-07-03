3829283d5f72bbc7752bd4f2cde223240e140b6a
Mike <mickael.ha@gmai.com>
2023-07-03 15:12:07 +0200

    Ajout de la blochain de test Sépolia dans le config

--- DAPP/TP03/backend/hardhat.config.js
+++ DAPP/TP03/backend/hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+const { INFURA_API_KEY, SEPOLIA_PRIVATE_KEY } = process.env
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    hardhat: {
+      blockGasLimit: 3000000 // ! Default 30_000_000
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    },
+  },
+  gasReporter: {
+    enabled: true,
+  },
 };
